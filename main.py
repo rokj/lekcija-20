99d9000c2dba9ff688e7cde0b00060d7e5ea661f
Matej Ramuta <matej.ramuta@gmail.com>
2015-07-27 16:38:54 +0200

    returns added to main.py

--- main.py
+++ main.py
@@ -11,25 +11,25 @@ jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), aut
 class BaseHandler(webapp2.RequestHandler):
 
     def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
+        return self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
 
     def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
+        return self.write(self.render_str(template, **kw))
 
     def render_template(self, view_filename, params=None):
         if not params:
             params = {}
         template = jinja_env.get_template(view_filename)
-        self.response.out.write(template.render(params))
+        return self.response.out.write(template.render(params))
 
 
 class MainHandler(BaseHandler):
     def get(self):
-        self.render_template("hello.html")
+        return self.render_template("hello.html")
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
